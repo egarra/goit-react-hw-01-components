b26cdeba9057223f94cae260bcab0c7a246eb9ad
egarra <garikhabuzov@gmail.com>
2022-12-08 20:09:12 +0200

    project create

---
+++ src/components/Statistics.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import '../css/Statistics.css';
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+function Statistics ({title, stats}) {
+    return (
+        <section className="statistics">
+            <h2 className="title">{title || null}</h2>
+            <div className='wrapper'>
+                <ul className="stat-list">
+                    {stats.map(({id, label, percentage}) => {
+                        return (
+                        <li 
+                            key={id} 
+                            className="item" 
+                            style={{background: getRandomHexColor()}}>
+                            <span className="label">{label}</span>
+                            <span className="percentage">{percentage}%</span>
+                        </li>   
+                        )
+                    })}
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.array.isRequired
+  }
+
+export default Statistics; 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import user from './user.json';
+import data from './data.json';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
 
+console.log(data)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Profile 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+    <Statistics
+      title="Upload stats"
+      stats={data}
+    />
+    <Statistics
+      stats={data}
+    />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
